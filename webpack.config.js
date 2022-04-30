345236882135db5a4047f5a27b9b9f7833584f2f
innteria <info.innteria@gmail.com>
2022-04-30 12:07:57 -0500

    webpack env

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
 module.exports = {
   entry: "./src/index.js",
   output: {
@@ -63,6 +65,7 @@ module.exports = {
         },
       ],
     }),
+    new Dotenv(),
   ],
   optimization: {
     minimize: true,
