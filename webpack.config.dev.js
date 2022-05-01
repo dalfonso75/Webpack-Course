47deb0af110f9643aa83f5c5ecfcaa6275794a4a
innteria <info.innteria@gmail.com>
2022-04-30 20:04:51 -0500

    webpack stats

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,6 +3,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
+const BundleAnalizerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   entry: "./src/index.js",
@@ -12,6 +14,7 @@ module.exports = {
     assetModuleFilename: "assets/images/[hash][ext][query]",
   },
   mode: "development",
+  devtool: "source-map",
   resolve: {
     extensions: [".js"],
     alias: {
@@ -65,6 +68,7 @@ module.exports = {
       ],
     }),
     new Dotenv(),
+    new BundleAnalizerPlugin(),
   ],
   devServer: {
     static: {
